954b8911fe76871a1cc97723a1685f8d669962f1
Vitaliy Murzin <murzinvit@yandex.ru>
2024-09-21 14:45:01 +0500

    add tomcat

---
+++ servlets/src/main/java/ru/netology/tomcat/TomcatLauncher.java
@@ -0,0 +1,36 @@
+package ru.netology.tomcat;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.startup.VersionLoggerListener;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class TomcatLauncher {
+    public static void main(String aArgs[]) {
+        try {
+            final Tomcat tomcat = new Tomcat();
+            final Path baseDir = Files.createTempDirectory("tomcat");
+            baseDir.toFile().deleteOnExit();
+            tomcat.setBaseDir(baseDir.toAbsolutePath().toString());
+
+            final Connector connector = new Connector();
+            connector.setPort(8080);
+            tomcat.setConnector(connector);
+
+            tomcat.getHost().setAppBase(".");
+            tomcat.addWebapp("",".");
+
+            tomcat.start();
+            tomcat.getServer().await();
+        } catch (LifecycleException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
